44014666fd6f9712edf17168cb81f2c283e96a4f
hasan huseyin <hkizilay65@gmail.com>
2021-11-20 12:40:00 +0300

    Coklu sart ornek degisti.

--- src/com/bilgeadam/sayisalloto/CokluSartOrnek.java
+++ src/com/bilgeadam/sayisalloto/CokluSartOrnek.java
@@ -7,12 +7,21 @@ public class CokluSartOrnek {
         int sayi1 = 23;
         int sayi2 = 35;
 
-        int sayi3 = 23;
+        int sayi3 = 41;
 
         // veya(or) opreratoru || isareti budur.
+        // ve (and) operatoru &&
         if (sayi3 == sayi1 || sayi3 == sayi2)
         {
             System.out.println("tekrarlanan sayi var!");
         }
+
+        while (sayi3 != sayi1 && sayi3 !=sayi2)
+        {
+            System.out.println("sayi3 digerlerine esit degil!");
+            sayi3 = sayi1;
+
+        }
+
     }
 }
